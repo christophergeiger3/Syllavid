2ddd4f899f51d33b80a6b58d2b85a4446286c5b0
Patricio <patpanico@hotmail.com>
2018-04-19 15:25:32 -0400

    Added Header Consistency

--- navbar.php
+++ navbar.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <title>Syllavid</title>
+        <!--<link rel="icon" type="image/png" href="assets/icon.png">-->
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
         <!-- Bootstrap relevant CDNs -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-        <!-- /Bootstrap relevant CDNs -->
+        
         <!-- Icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- /Icon library -->
+        
         <!-- JQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <!-- /JQuery -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="stylesheet.css">
-        <!--<link rel="icon" type="image/png" href="assets/icon.png">-->
-        <title>Syllavid</title>
     </head>
     <body>
         <nav class="navbar navbar-inverse">
