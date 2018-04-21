b2e2fbfca46d10eefbabd0b6f6f62a463e0dfc8f
christophergeiger3 <christophergeiger3@gmail.com>
2018-04-21 14:53:10 -0400

    Fixup contact.php

--- contact.php
+++ contact.php
@@ -15,7 +15,50 @@
     <body>
         <?php include_once('navbar.php'); ?>
         <br></br>
-        <h2 style="margin-left: 2%">Email:</h4>
-        <h3 style="margin-left: 2%">chris.dantonio@uconn.edu</h3>
+
+<div class="container" style="margin-left: 2%">
+	<div class="row">
+      <div class="col-md-6">
+        <div class="well well-sm">
+          <form class="form-horizontal" action="" method="post">
+          <fieldset>
+            <legend class="text-center">Contact us</legend>
+
+            <!-- Name input-->
+            <div class="form-group">
+              <label class="control-label" for="name">Name</label>
+              <div class="">
+                <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+              </div>
+            </div>
+
+            <!-- Email input-->
+            <div class="form-group">
+              <label class="control-label" for="email">Your E-mail</label>
+              <div class="">
+                <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+              </div>
+            </div>
+
+            <!-- Message body -->
+            <div class="form-group">
+              <label class="control-label" for="message">Your message</label>
+              <div class="">
+                <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+              </div>
+            </div>
+
+            <!-- Form actions -->
+            <div class="form-group">
+              <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+              </div>
+            </div>
+          </fieldset>
+          </form>
+        </div>
+      </div>
+	</div>
+</div>
     </body>
 </html>
