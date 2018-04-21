d985fadf37e8988199b87bd4650579f462a3e38f
christophergeiger3 <christophergeiger3@gmail.com>
2018-04-20 20:59:15 -0400

    Remove social media buttons

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
         <title>Syllavid</title>
         <!--<link rel="icon" type="image/png" href="assets/icon.png">-->
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
-        
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        
+
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -29,10 +29,6 @@
                 </p>
             </div>
             <div class="container">
-                <a href="#" class="fa fa-facebook"></a>
-                <a href="#" class="fa fa-twitter"></a>
-                <a href="#" class="fa fa-google"></a>
-                <a href="#" class="fa fa-linkedin"></a>
             </div>
         </section>
     </body>
