d31980cc367c344929226ff8196771cc43ba7380
christophergeiger3 <christophergeiger3@gmail.com>
2018-04-21 11:58:55 -0400

    Add .thumbnail to css

--- example.php
+++ example.php
@@ -4,10 +4,10 @@
         <title>Syllavid</title>
         <!--<link rel="icon" type="image/png" href="assets/icon.png">-->
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
-        
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        
+
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -17,7 +17,6 @@
         <?php include_once('navbar.php'); ?>
         <section>
             <div class="container">
-
             </div>
             <br></br>
             <div class="container">
@@ -29,7 +28,7 @@
                   </tr>
                   <tr>
                     <td>Unit 2: Forecasting</td>
-                    <td>YouTube videos</td>
+                    <!-- <td>YouTube videos</td> -->
                   </tr>
                   <tr>
                     <td>Unit 3: Project Management</td>
