064064334a35bbae716b705ada1626bf13d7902b
christophergeiger3 <christophergeiger3@gmail.com>
2018-04-21 13:19:12 -0400

    Fix searchbar

--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@
           		<div class="input-group row">
                 <a href="courses" class="btn btn-primary mr-2">Video Courses</a>
           			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-        				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+        				<button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
           		</div>
         		</form>
         </nav>
