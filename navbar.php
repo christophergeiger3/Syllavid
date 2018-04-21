5cece24c38461b7c9c7a140f85371b5648e668a0
Patricio <patpanico@hotmail.com>
2018-04-21 14:35:12 -0400

    Search Bar

--- navbar.php
+++ navbar.php
@@ -6,6 +6,13 @@
                 <a class="navbar-brand" style="font-size: 1.5em" href="/">Syllavid</a>
             </div>
             <a href="/courses" class="btn btn-primary">Video Courses</a>
+            <form class="navbar-form" role="search">
+                <div class="input-group row">
+                    <a href="courses" class="btn btn-primary mr-2">Video Courses</a>
+                    <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                    <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
         </nav>
     </body>
 </html>
