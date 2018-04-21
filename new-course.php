00007b4d37090bc35a950502f3869e7c647ce0a4
Patricio <patpanico@hotmail.com>
2018-04-21 10:47:59 -0400

    Add new-course Page

---
+++ new-course.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Syllavid</title>
+        <!--<link rel="icon" type="image/png" href="assets/icon.png">-->
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <?php include_once('navbar.php'); ?>
+        <section class="jumbotron p-4">
+          <br></br>
+            <div class="container">
+                <img src="img/elon_musk.gif" class="mx-auto img-responsive" id="main_img" width="600" />
+            </div>
+          <br></br>
+        </section>
+        <section class="text-center" id="main_logo">
+            <div class="container">
+                <h1 class="jumbotron-heading">Syllavid</h1>
+                <p class="lead text-muted">Resources for your courses, free, forever.</p>
+                    <a href="#" class="btn btn-primary my-2">Learn More</a>
+                    <a href="#" class="btn btn-secondary my-2">Contact Us</a>
+            </div>
+        </section>
+    </body>
+    <footer class="footer">
+      <a href="#"><span class="text-muted float-right" style="margin-right: 1%">Terms of Use</span></a>
+      <a href="#"><span class="text-muted float-right" style="margin-right: 2%">Privacy Policy</span></a>
+      <a href="#"><span class="text-muted float-right" style="margin-right: 2%">Donate</span></a>
+      <a href="#"><span class="text-muted float-right" style="margin-right: 2%">About</span></a>
+    </footer>
+</html>
+
