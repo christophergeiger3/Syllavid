911ebe7a36e6688d07d0f964cb538dea3119d987
christophergeiger3 <christophergeiger3@gmail.com>
2018-04-21 13:14:37 -0400

    Add navbar

--- navbar.php
+++ navbar.php
@@ -5,7 +5,13 @@
             <div class="navbar-header">
                 <a class="navbar-brand" style="font-size: 1.5em" href="/">Syllavid</a>
             </div>
-            <a href="courses" class="btn btn-primary">Video Courses</a>
+        		<form class="navbar-form" role="search">
+          		<div class="input-group row">
+                <a href="courses" class="btn btn-primary mr-2">Video Courses</a>
+          			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+        				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+          		</div>
+        		</form>
         </nav>
     </body>
 </html>
