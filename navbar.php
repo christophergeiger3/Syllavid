78d626a304f0ac654ad35eba9c6b49aa65757661
Patricio <patpanico@hotmail.com>
2018-04-21 14:30:29 -0400

    Changed Navbar Shadow

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <body>
-        <nav class="navbar navbar-inverse">
+        <nav class="navbar shadow">
             <div class="navbar-header">
                 <a class="navbar-brand" style="font-size: 1.5em" href="/">Syllavid</a>
             </div>
-            <a href="courses" class="btn btn-primary">Video Courses</a>
+            <a href="/courses" class="btn btn-primary">Video Courses</a>
         </nav>
     </body>
 </html>
